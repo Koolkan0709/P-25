4cd4df470786b658224e77e1ab9ec81eb553ecf1
Koolkan <75653679+Koolkan0709@users.noreply.github.com>
2021-09-14 16:14:36 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -11,7 +11,8 @@ class Paper{
 		this.y=y;
 		this.r=r
 		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options);
-		World.add(world, this.body);
+		this.image = loadImage("paper.png")
+        World.add(world, this.body);
 
 	}
 	display()
@@ -23,8 +24,10 @@ class Paper{
 			strokeWeight(4);
 			stroke("gray");
 			fill("white");
-			ellipseMode(CENTER)
-			ellipse(0, 0, this.r, this.r);
+			imageMode(CENTER);
+			image(this.image,0,0,this.r,this.r);
+			//ellipseMode(CENTER)
+			//ellipse(0, 0, this.r, this.r);
 			pop()
 	}
 
